8219e02c192ba942bf2449a600a177fcb6efce00
李志刚 <lizhigang@lizhigangdeMacBook-Pro.local>
2022-04-20 15:36:08 +0800

    1.0版本

---
+++ Example/Pods/Target Support Files/MARSDK_ios/MARSDK_ios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MARSDK_ios : NSObject
+@end
+@implementation PodsDummy_MARSDK_ios
+@end
---
+++ Example/Pods/Target Support Files/Pods-MARSDK_ios_Example/Pods-MARSDK_ios_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MARSDK_ios_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MARSDK_ios_Example
+@end
